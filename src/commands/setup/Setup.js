7b9143400c882268e2ce970c9970125ece28db45
BuildTools <unconfigured@null.spigotmc.org>
2021-07-17 01:48:02 -0600

    Setup commands

--- src/commands/setup/Setup.js
+++ src/commands/setup/Setup.js
@@ -29,7 +29,29 @@ class Setup extends Command {
                 await sh("php artisan migrate --seed --force");
                 await sh("php artisan p:user:make");
                 await sh("chown -R www-data:www-data /var/www/pterodactyl/*");
-
+                await sh(`(crontab -u root -l ; echo "* * * * * php /var/www/pterodactyl/artisan schedule:run >> /dev/null 2>&1") | crontab -u root -`)
+                await sh("crontab -u root -l");
+                console.title("Adding Pterodactyl service...");
+                const pService = `# Pterodactyl Queue Worker File
+                # ----------------------------------
+                
+                [Unit]
+                Description=Pterodactyl Queue Worker
+                After=redis-server.service
+                
+                [Service]
+                # On some systems the user and group might be different.
+                # Some systems use 'apache' or 'nginx' as the user and group.
+                User=www-data
+                Group=www-data
+                Restart=always
+                ExecStart=/usr/bin/php /var/www/pterodactyl/artisan queue:work --queue=high,standard,low --sleep=3 --tries=3
+                
+                [Install]
+                WantedBy=multi-user.target" > /etc/systemd/system`
+                fs.writeFileSync("/etc/systemd/system/pteroq.service", pService);
+                await sh("systemctl enable --now redis-server");
+                await sh("systemctl enable --now pteroq.service")
             }
             catch (e) {
                 console.printError(e);
